0090ccccde6bf5c446f13fff8ed725a21bda93dd
ujjwalsharma25 <ujjwal.sharma.cse.2024@miet.ac.in>
2026-09-21 22:29:43 +0530

    Chapter 12

--- Chapter 12 Dyanamic Ui Using EJS/app.js
+++ Chapter 12 Dyanamic Ui Using EJS/app.js
@@ -6,22 +6,26 @@ const express = require('express');
 
 //Local Module
 const userRouter = require("./routes/userRouter")
-const hostRouter = require("./routes/hostRouter")
-const rootDir = require("./utils/pathUtils");
+const {hostRouter} = require("./routes/hostRouter")
+const rootDir = require("./utils/pathUtil");
 
 const app = express();
 
-app.use(express.static(path.join(rootDir, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(express.urlencoded());
 app.use(userRouter);
 app.use("/host", hostRouter);
 
+app.use(express.static(path.join(rootDir, 'public')))
+
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 })
 
-/* const PORT = 5002;
+
+const PORT = 5002;
 app.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
-}); */
+});
